64a161b6b28015bbe7afef88298e00d2116a8df1
eucandre <eucandre@gmail.com>
2017-12-21 02:19:03 -0200

    Criada regra na views para que sejam retornadas as duas ultimas noticias

--- app_documentos/views.py
+++ app_documentos/views.py
@@ -5,10 +5,11 @@ def Apresentacao(request):
 	"""
 		Este metodo sera o responsavel pela Apresentacao do sistema.
 		Aqui as variaveis do sistema serao chamadas para exibir os seus resultados.
+		O metodo irah retornar, para o template, os dois ultimos registros no banco fazendo uso da variavel obj_noticias
 	"""
 
-	obj_noticias = Noticias.objects.all()
-	if len(obj_noticias)>0:
+	obj_noticias = Noticias.objects.all()[(len(Noticias.objects.all())-2):]
+	if len(obj_noticias) > 0:
 		return render (request, 'apps/index.html',{"noticias":obj_noticias})
 	else:
 		return render (request, 'apps/index.html')
