9589d477d533c18076f9dfbe37586afa6baa3cf1
eucandre <eucandre@gmail.com>
2018-01-26 13:50:56 -0200

    Commit semiautom├ítico

---
+++ app_documentos/item.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import *
+from .forms import *
+from django.http import *
+
+def itemNoticia(request, nr_item):
+    try:
+        titulo = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('titulo')
+        #titulo = Noticias.objects.get(pk = nr_item)
+        categoria = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('categoria_da_noticia')
+        data = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('data_publicacao')
+        destaque = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('destaque')
+        publicado = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('publicado')
+        imagem = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('imagem')
+        video = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('video')
+        credito_midia_imagem = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('credito_midia_imagem')
+        legenda_da_imagem = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('legenda_da_imagem')
+        credito_midia_video = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('credito_midia_video')
+        legenda_do_video = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('legenda_do_video')
+        documentos_complementares = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('documentos_complementares')
+        chapeu = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('chapeu')
+        bigode = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('bigode')
+        reporter = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('reporter')
+        tags_lista = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('tags_lista')
+        nota = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('nota')
+        user = Noticias.objects.all().values()[len(Noticias.objects.all())-len(Noticias.objects.all())].get('user')
+    except Noticias.DoesNotExist:
+        raise Http404('Sem Registro!')
+    return render(request, "app_capacitacoes/item_noticias.html", {'titulo': titulo, 'categoria':categoria, 'data':data
+    	,'destaque':destaque,'publicado':publicado, 'imagem':imagem,'video':video, 'credito_midia_imagem':credito_midia_imagem,
+    	'credito_midia_video':credito_midia_video,'legenda_da_imagem':legenda_da_imagem,'legenda_do_video':legenda_do_video,
+    	'documentos_complementares':documentos_complementares,'chapeu':chapeu, 'bigode':bigode, 'reporter':reporter, 'tags_lista':tags_lista})
--- portal_sei/urls.py
+++ portal_sei/urls.py
@@ -5,6 +5,7 @@ from app_documentos.views import *
 from app_documentos.insere import *
 from app_documentos.edita import *
 from app_documentos.deleta import *
+from app_documentos.item import *
 from django.conf import settings
 from django.conf.urls.static import static
 from app_documentos.api import *
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^lista_documentos', lista_documentos),
     url(r'^lista_capacitacoes', lista_capacitacoes),
     url(r'^lista_noticias', lista_noticias),
+    url(r'^noticias/(?P<nr_item>\d+)/$',itemNoticia),
     url(r'^EditarDocumento/(?P<nr_item>\d+)/$',EditaDocumento),
     url(r'^Delete_Documento/(?P<nr_item>\d+)/$', DocumentoDelete),
 
